68ea51cf730549fa40bad24eedbac653fc6ea29f
alan_aleks <alan_aleks@mail.ru>
2024-11-20 14:57:34 +0500

    Oauth2 approval_prompt force

--- example/index.php
+++ example/index.php
@@ -6,7 +6,9 @@ use GoodGame\Oauth2\Factory;
 
 //$provider = Factory::getTest(require 'config.php');
 $provider = Factory::getReal(require 'config.php');
-$url = $provider->getAuthorizationUrl();
+$url = $provider->getAuthorizationUrl([
+    'approval_prompt' => 'auto', // 'force',
+]);
 ?>
 <a href="<?= $url ?>">На эту ссылку нужно переадресовать вашего пользователя.
     Сейчас вручную перейдите по ссылке
